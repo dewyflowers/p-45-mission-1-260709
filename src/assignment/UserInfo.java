999760b9874fbebb5376485ff56ef0943655dfa7
dewyflowers <hyeseung.dev@gmail.com>
2026-07-09 15:13:12 +0900

    Add UserInfo class and gitignore

---
+++ src/assignment/UserInfo.java
@@ -0,0 +1,10 @@
+package assignment;
+
+import java.sql.SQLOutput;
+
+public class UserInfo {
+    public static void main(String[] args) {
+        System.out.println("제 이름은 백혜승입니다.");
+        System.out.println("제 나이는 27입니다.");
+    }
+}
